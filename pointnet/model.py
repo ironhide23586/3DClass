🔥 was using validation as trianing - fixed that and fixed random rotation problem

pointnet/model.py
@@ -72,9 +72,9 @@ class PointNet:
         self.model.load_weights(fpath)
 
     def train(self):
-        # x, y = self.keras_train_data.__getitem__(0)
-        # rgbs = utils_.new_colors[y[0]]
-        # utils_.write_ply('tmp_.ply', x, rgbs)
+        x, y = utils_.sample_data(self.train_point_data, random_transform=True)
+        rgbs = utils_.new_colors[y[0]]
+        utils_.write_ply('tmp_.ply', x, rgbs)
 
         exps_dec = tf.keras.optimizers.schedules.ExponentialDecay(utils_.BASE_LR, utils_.NUM_TRAIN_STEPS,
                                                                   utils_.LR_EXP_DECAY_POWER)
@@ -114,12 +114,12 @@ class PointNet:
             return ret
 
     def get_val_gen(self):
-        for _ in range(utils_.BATCHES_PER_EPOCH):
+        for _ in range(10):
             yield utils_.sample_data(self.val_point_data, random_transform=False)
 
     def get_train_gen(self):
         for _ in range(utils_.NUM_TRAIN_STEPS):
-            yield utils_.sample_data(self.val_point_data, random_transform=True)
+            yield utils_.sample_data(self.train_point_data, random_transform=True)
 
     def make_model(self):
         # (1) input

utils_.py
@@ -117,8 +117,9 @@ def xyz_preprocess(xyzs_, translate=False):
     else:
         xyzs = xyzs_
     # cxyz = np.array([GRID_W / 2., GRID_H / 2., GRID_W / 2.])
-    cxyz = np.array([POINT_TILER_SIDE / 2., POINT_TILER_SIDE / 2., POINT_TILER_SIDE / 2.])
-    return (xyzs - cxyz) / cxyz
+    c = GRID_D / GRID_W
+    cxyz = np.array([POINT_TILER_SIDE / 2., POINT_TILER_SIDE / 2., 0.])
+    return (xyzs - cxyz) / np.array([POINT_TILER_SIDE / 2., POINT_TILER_SIDE / 2., c * POINT_TILER_SIDE / 2.])
 
 
 def z2rgb(zs_, min_z=-600, max_z=120):