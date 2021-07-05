cac6a9cf645bcd15a5f3e0c5ca0225685d255d64
Souham Biswas <souham.biswas@outlook.com>
2021-07-05 18:00:36 +0530

    ⚡🛠 now processing entire sets of LAZ

--- infer.py
+++ infer.py
@@ -12,10 +12,12 @@ Website: https://www.linkedin.com/in/souham/
 """
 
 import os
+from glob import glob
+
 from concurrent.futures import ThreadPoolExecutor
 from multiprocessing import cpu_count
 
-os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
 import numpy as np
 
@@ -23,17 +25,10 @@ import utils_
 from data_io import LAZElem
 from pointnet.model import PointNet
 
-LAZ_FPATH = utils_.DIR + os.sep + 'C_37EZ1_0_2.laz'
-prefix = LAZ_FPATH.split(os.sep)[-1].replace('.laz', '')
-out_dir = LAZ_FPATH + '-outs'
-
-laz_data = LAZElem(LAZ_FPATH)
-laz_data.load()
-pnet = PointNet(mode='infer')
-pnet.load_weights('aerial-pointnet-weights.19-1.24.hdf5')
+# LAZ_FPATH = utils_.DIR + os.sep + 'C_37EZ1_0_2.laz'
 
 
-def clf_pcl(xy):
+def clf_pcl(xy, pnet, out_dir, prefix, laz_data):
     suffix = '-'.join(map(str, xy))
     fp = out_dir + os.sep + prefix + '_' + suffix + '.ply'
     if os.path.exists(fp):
@@ -69,6 +64,20 @@ def clf_pcl(xy):
         utils_.write_ply(fp, tile_xyzs_raw + t_vec, labels_pred_rgb)
 
 
+def process_laz(fpath, pnet):
+    laz_data = LAZElem(fpath)
+    prefix = LAZ_FPATH.split(os.sep)[-1].replace('.laz', '')
+    out_dir = LAZ_FPATH + '-outs'
+    laz_data.load()
+    nx, ny = laz_data.num_xy_tiles
+    tile_centers = np.rollaxis(np.array(np.meshgrid(np.arange(nx), np.arange(ny))), 0, 3).reshape([-1, 2])
+    utils_.force_makedir(out_dir)
+    ex = ThreadPoolExecutor(max_workers=cpu_count())
+    for tc in tile_centers:
+        ex.submit(clf_pcl, tc, pnet, out_dir, prefix, laz_data)
+    ex.shutdown()
+
+
 if __name__ == '__main__':
 
     # ret = laz_data.sample_tile([22, 1])
@@ -84,43 +93,8 @@ if __name__ == '__main__':
     # labels_pred_rgb = utils_.new_colors[labels]
     # utils_.write_ply(fp, tile_xyzs, labels_pred_rgb)
 
-    nx, ny = laz_data.num_xy_tiles
-    tile_centers = np.rollaxis(np.array(np.meshgrid(np.arange(nx), np.arange(ny))), 0, 3).reshape([-1, 2])
-
-    utils_.force_makedir(out_dir)
-
-    # for tc in tile_centers:
-    #     clf_pcl(tc)
-
-    # p = Pool(cpu_count())
-    # p.map(clf_pcl, tile_centers)
-
-    ex = ThreadPoolExecutor(max_workers=cpu_count())
-    for tc in tile_centers:
-        ex.submit(clf_pcl, tc)
-    ex.shutdown()
-    k = 0
-
-    # suffix = '-'.join(map(str, [laz_data.start_tile_x, laz_data.start_tile_y]))
-    # tile_xyzs_raw = laz_data.sample_tile()
-    # while tile_xyzs_raw is not None:
-    #     tile_xyzs = utils_.xyz_preprocess(tile_xyzs_raw)
-    #     tile_pred = pnet.infer(tile_xyzs).argmax(axis=-1)
-    #
-    #     labels_pred_rgb = utils_.new_colors[tile_pred]
-    #     fp = out_dir + os.sep + prefix + '_' + suffix + '.ply'
-    #     utils_.write_ply(fp, tile_xyzs_raw + laz_data.t_vec, labels_pred_rgb)
-    #
-    #     suffix = '-'.join(map(str, [laz_data.start_tile_x, laz_data.start_tile_y]))
-    #     tile_xyzs_raw = laz_data.sample_tile()
-
-    k = 0
-
-    #
-    # xyzs = ps.read_laz(LAZ_FPATH)
-    #
-    # shift_point = xyzs_.min(axis=0)
-    # xyzs = xyzs_ - shift_point
-    # scale_point = xyzs.max(axis=0)
-    # xyzs = xyzs / scale_point
-    #
+    pnet = PointNet(mode='infer')
+    pnet.load_weights('aerial-pointnet-weights.19-1.24.hdf5')
+    laz_fpaths = glob(utils_.DIR + os.sep + '*.laz')
+    for laz_fp in laz_fpaths:
+        process_laz(laz_fp, pnet)
